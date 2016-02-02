b755ecfe67a270503629acea38b027462e09c9aa
yassu <mathyassu@gmail.com>
2016-02-02 11:15:24 +0900

    good cells and layout

--- src/roque.py
+++ src/roque.py
@@ -16,12 +16,16 @@ class Bord(QtGui.QWidget):
 
     def initUI(self):
         grid = QtGui.QGridLayout()
+        grid.setSpacing(1)
         self.setLayout(grid)
         self.cell_mat = [
             [0 for y in range(CELL_MAX_Y)] for x in range(CELL_MAX_X)]
         for i in range(CELL_MAX_Y):
             for j in range(CELL_MAX_X):
-                self.cell_mat[i][j] = QtGui.QPushButton('({}, {})'.format(i, j))
+                self.cell_mat[i][j] = QtGui.QPushButton()
+                self.cell_mat[i][j].setStyleSheet("background-color: black")
+                self.cell_mat[i][j].setFixedWidth(32)
+                self.cell_mat[i][j].setFixedHeight(32)
                 grid.addWidget(self.cell_mat[i][j], i, j)
 
 def main():
